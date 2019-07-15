87e383f7ac5a4437aff42d88b2965679d7476622
Travis Simpkins <t73designs@yahoo.com>
2019-07-15 11:23:12 -0400

    Final

--- mail.php
+++
@@ -1,5 +0,0 @@
-<?php
-if($_POST["message"]) {
-    mail("t73designs@yahoo.com", "Form to email message", $_POST[Name: "#firstName" "#lastName" + Company: "#company" + "message"], "From: an@email.address");
-}
-?>
