0f7c2fbbdb2b6580af11fb1707a558d7b19d961f
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2020-07-28 22:52:30 -0400

    React Changes

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -42,7 +42,7 @@ export default class Carousel extends React.Component {
             return (
                 <div className="carousel">
                     <Arrow direction="left" clickFunction={ this.previousSlide } glyph="&#9664;" />
-                    <ImageSlide url={ imgUrls[this.state.currentImageIndex] } />
+                    <ImageSlide project={ this.props.projects[this.state.currentImageIndex] } />
                     <Arrow direction="right" clickFunction={ this.nextSlide } glyph="&#9654;" />
                 </div>
             );
@@ -57,15 +57,23 @@ export default class Carousel extends React.Component {
         </div>
     );
     
-    const ImageSlide = ({ url }) => {
+    const ImageSlide = ({ project }) => {
         const styles = {
-            backgroundImage: `url(${url})`,
+            backgroundImage: `url(${project.img})`,
             backgroundSize: 'cover',
             backgroundPosition: 'center'
         };
         
         return (
-            <div className="image-slide" style={styles}></div>
+            <div className="image-slide" style={styles}>
+                <h2>{project.title}</h2>
+          <img class="project-pic" src=""{project.img}"" alt="Web Dev Toolkit App Project" />
+         {project.description}
+          <a href=""{project.liveLink}"" class="portfolioLinks"
+             target="_blank">View Live Project</a>
+          <a href=""{project.repoLink}"" class="portfolioLinks"
+              target="_blank">View Project Repo</a>
+            </div>
         );
     }
     
